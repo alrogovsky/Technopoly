a2d1c35388b052328adf5d4f4ad29594a6d3fd51
apanichkina <anna@MacBook-Air-ANNA.local>
2015-06-07 18:52:06 +0300

    fix bug

--- Classes/MainTableScene.cpp
+++ Classes/MainTableScene.cpp
@@ -274,9 +274,7 @@ void MainTable::menuCloseCallback(Ref* pSender)
     warpClientRef->disconnect();
     Director::getInstance()->end();
     
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    exit(0);
-#endif
+
 }
 
 void MainTable::onRotateRight(Ref* pSender)
@@ -284,19 +282,14 @@ void MainTable::onRotateRight(Ref* pSender)
     float current_rotation = table->getRotation();
     table->setRotation(current_rotation + 90);
     
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    exit(0);
-#endif
+
 }
 
 void MainTable::onRotateLeft(Ref* pSender)
 {
     float current_rotation = table->getRotation();
     table->setRotation(current_rotation - 90);
-    
-#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
-    exit(0);
-#endif
+
 }
 
 void MainTable::onTest(cocos2d::Ref *pSender)
